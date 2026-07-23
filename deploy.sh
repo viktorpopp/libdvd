f916782339a42430dd27cc7886fa7e2d6f25b3b2
Viktor Popp <vpopp@proton.me>
2026-07-23 18:45:05 +0200

    dont detach on deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ echo "Running database migrations..."
 docker compose --env-file .env -f $COMPOSE_FILE run --rm app pnpm prisma migrate deploy
 
 echo "Restarting containers..."
-docker compose --env-file .env -f $COMPOSE_FILE up -d
+docker compose --env-file .env -f $COMPOSE_FILE up
 
 echo "Deployed application!"
