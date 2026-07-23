b5e1d0fd02c5cb70929c11535c51b6a64cfbc4ed
Viktor Popp <vpopp@proton.me>
2026-07-23 16:57:59 +0200

    make deployment server only

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-: "${SERVER:="viktor@homelab"}"
-: "${REMOTE_DIR:="/home/viktor/libdvd"}"
 : "${COMPOSE_FILE:="docker/docker-compose.prod.yml"}"
 
 echo "Starting Deployment..."
@@ -11,30 +9,10 @@ echo "Starting Deployment..."
 echo "Building Docker images..."
 docker compose --env-file .env -f $COMPOSE_FILE build
 
-echo "Saving application image..."
-docker save libdvd-app:latest -o /tmp/libdvd-images.tar
+echo "Running database migrations..."
+docker compose --env-file .env -f $COMPOSE_FILE run --rm app pnpm prisma migrate deploy
 
-echo "Transferring application image to server..."
-scp /tmp/libdvd-images.tar $SERVER:/tmp/libdvd-images.tar
-scp $COMPOSE_FILE $SERVER:$REMOTE_DIR/
-
-ssh $SERVER << EOF
-    set -e
-    cd /home/viktor/libdvd
-    git pull
-
-    echo "Loading Docker images..."
-    docker load -i /tmp/libdvd-images.tar
-    rm /tmp/libdvd-images.tar
-
-    echo "Running database migrations..."
-    docker compose --env-file .env -f docker/docker-compose.prod.yml run --rm app pnpm prisma migrate deploy
-
-    echo "Restarting containers..."
-    docker compose --env-file .env -f docker/docker-compose.prod.yml up -d
-EOF
-
-echo "Removing locally saved image..."
-rm /tmp/libdvd-images.tar
+echo "Restarting containers..."
+docker compose --env-file .env -f $COMPOSE_FILE up -d
 
 echo "Deployed application!"
