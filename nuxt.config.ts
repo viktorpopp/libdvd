591b5aa21a40f7144bd54ce9bbfb2b19c67917e5
Viktor Popp <vpopp@proton.me>
2026-07-23 14:33:52 +0200

    fallback sdfhjkasdflhjka

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,11 +9,11 @@ export default defineNuxtConfig({
     },
   },
   runtimeConfig: {
-    s3AccessKey: process.env.RUSTFS_ACCESS_KEY,
-    s3SecretKey: process.env.RUSTFS_SECRET_KEY,
-    s3Endpoint: process.env.RUSTFS_ENDPOINT,
-    s3Region: process.env.RUSTFS_REGION,
-    s3BucketName: process.env.RUSTFS_BUCKET_NAME,
+    s3AccessKey: process.env.RUSTFS_ACCESS_KEY || "",
+    s3SecretKey: process.env.RUSTFS_SECRET_KEY || "",
+    s3Endpoint: process.env.RUSTFS_ENDPOINT || "http://rustfs:9000",
+    s3Region: process.env.RUSTFS_REGION || "eu-west-1",
+    s3BucketName: process.env.RUSTFS_BUCKET_NAME || "images",
     public: {
       authBaseUrl:
         process.env.NUXT_PUBLIC_AUTH_BASE_URL || "http://localhost:3000",
